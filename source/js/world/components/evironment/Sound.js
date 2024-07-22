a6936ced46efaa8ea41e644427c205d7fac9e3a5
howardjwilliamson <75391134+howardjwilliamson@users.noreply.github.com>
2024-07-23 00:50:58 +0100

    Updated file references.

--- source/js/world/components/evironment/Sound.js
+++ source/js/world/components/evironment/Sound.js
@@ -2,14 +2,14 @@
 
 export const Solfege = 
 {
-    "a": { url:'../../../../../assets/audio/solfege/notes/katy/note060-do.wav', volum: 1 },
-    "b": { url:'../../../../../assets/audio/solfege/notes/katy/note062-re.wav', volum: 1 },
-    "c": { url:'../../../../../assets/audio/solfege/notes/katy/note064-mi.wav', volum: 1 },
-    "d": { url:'../../../../../assets/audio/solfege/notes/katy/note065-fa.wav', volum: 1 },
-    "e": { url:'../../../../../assets/audio/solfege/notes/katy/note067-so.wav', volum: 1 },
-    "f": { url:'../../../../../assets/audio/solfege/notes/katy/note069-la.wav', volum: 1 },
-    "g": { url:'../../../../../assets/audio/solfege/notes/katy/note071-ti.wav', volum: 1 },
-    "A": { url:'../../../../../assets/audio/solfege/notes/katy/note072-do.wav', volum: 1 },
+    "a": { url:'./../../../../../assets/audio/solfege/notes/katy/note060-do.wav', volum: 1 },
+    "b": { url:'./../../../../../assets/audio/solfege/notes/katy/note062-re.wav', volum: 1 },
+    "c": { url:'./../../../../../assets/audio/solfege/notes/katy/note064-mi.wav', volum: 1 },
+    "d": { url:'./../../../../../assets/audio/solfege/notes/katy/note065-fa.wav', volum: 1 },
+    "e": { url:'./../../../../../assets/audio/solfege/notes/katy/note067-so.wav', volum: 1 },
+    "f": { url:'./../../../../../assets/audio/solfege/notes/katy/note069-la.wav', volum: 1 },
+    "g": { url:'./../../../../../assets/audio/solfege/notes/katy/note071-ti.wav', volum: 1 },
+    "A": { url:'./../../../../../assets/audio/solfege/notes/katy/note072-do.wav', volum: 1 },
 };
 
 export const Notes = ["a", "b", "c", "d", "e", "f", "g", "A"];
